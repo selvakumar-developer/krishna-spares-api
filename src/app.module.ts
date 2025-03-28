37ff08145550a0daf13476a99ee4b36ca0339361
selvakumar-developer <selvatechy7@gmail.com>
2025-03-28 12:05:12 +0530

    KAN-21 Enable GraphQL introspection in the application module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AdminUsersModule } from './admin-users/admin-users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import configuration from './config/configuration';
 import { IAppConfig } from './interface/config';
 import { UsersModule } from './users/users.module';
-import { AdminUsersModule } from './admin-users/admin-users.module';
 
 @Module({
   imports: [
@@ -30,6 +30,7 @@ import { AdminUsersModule } from './admin-users/admin-users.module';
       autoSchemaFile: true,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       playground: false,
+      introspection: true
       // formatError(formattedError, error) {
       //   return {
 
@@ -44,4 +45,4 @@ import { AdminUsersModule } from './admin-users/admin-users.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
